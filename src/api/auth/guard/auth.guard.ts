fc7ae8ea5fefce215d613ca5dca39b711a02955e
Rafa-VP <rvaca@autored.cl>
2023-07-09 14:43:12 -0500

    feat: :sparkles: CORS
    Enabling CORS

--- src/api/auth/guard/auth.guard.ts
+++ src/api/auth/guard/auth.guard.ts
@@ -20,7 +20,6 @@ export class AuthGuard implements CanActivate {
       context.getClass()
     ])
     if (isPublic) {
-      // 💡 See this condition
       return true
     }
     const request = context.switchToHttp().getRequest()
@@ -32,8 +31,6 @@ export class AuthGuard implements CanActivate {
       const payload = await this.jwtService.verifyAsync(token, {
         secret: jwtConstants.secret
       })
-      // 💡 We're assigning the payload to the request object here
-      // so that we can access it in our route handlers
       request['user'] = payload
     } catch {
       throw new UnauthorizedException()
--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,7 @@ import { ValidationPipe } from '@nestjs/common'
 async function main() {
   const app = await NestFactory.create(AppModule)
   app.useGlobalPipes(new ValidationPipe())
+  app.enableCors({ origin: '*', methods: ['POST', 'PUT', 'DELETE', 'GET'] })
   await app.listen(3000)
 }
 main()
