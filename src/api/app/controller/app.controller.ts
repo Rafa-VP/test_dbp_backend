4c7d4e538b534e18b6ef2b9981050bb145b05a92
Rafa-VP <rvaca@autored.cl>
2023-07-09 13:36:39 -0500

    fix: :bug: Fix Public Route Bug
    Fix Public Route Bug

--- src/api/app/controller/app.controller.ts
+++ src/api/app/controller/app.controller.ts
@@ -1,11 +1,12 @@
 import { Controller, Get } from '@nestjs/common'
 import { AppService } from '../service/app.service'
 import { Public } from 'src/api/decorator/custom.decorator'
-@Public()
+
 @Controller('/')
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
+  @Public()
   @Get()
   getHello(): object {
     return this.appService.getHello()
