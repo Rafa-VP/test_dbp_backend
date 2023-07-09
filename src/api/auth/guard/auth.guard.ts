034845e6f98c46a1eea3bdc8f4adbe87225723bb
Rafa-VP <rvaca@autored.cl>
2023-07-09 18:32:09 -0500

    fix:

--- src/api/auth/guard/auth.guard.ts
+++ src/api/auth/guard/auth.guard.ts
@@ -13,7 +13,6 @@ import { IS_PUBLIC_KEY } from '../../decorator/custom.decorator'
 @Injectable()
 export class AuthGuard implements CanActivate {
   constructor(private jwtService: JwtService, private reflector: Reflector) {}
-
   async canActivate(context: ExecutionContext): Promise<boolean> {
     const isPublic = this.reflector.getAllAndOverride<boolean>(IS_PUBLIC_KEY, [
       context.getHandler(),
