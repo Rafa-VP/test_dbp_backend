20d9f2ba2a821010ef8f07d8fb4314bb29571fc3
Rafa-VP <rvaca@autored.cl>
2023-07-09 13:34:35 -0500

    feat: :lock: Adding main route to public
    Adding main route to public

--- src/api/app/controller/app.controller.ts
+++ src/api/app/controller/app.controller.ts
@@ -1,6 +1,7 @@
 import { Controller, Get } from '@nestjs/common'
 import { AppService } from '../service/app.service'
-
+import { Public } from 'src/api/decorator/custom.decorator'
+@Public()
 @Controller('/')
 export class AppController {
   constructor(private readonly appService: AppService) {}
