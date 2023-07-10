db14514ae1aec351020706dcf13c524e539eb5a7
Rafa-VP <rafaelvak9@gmail.com>
2023-07-09 20:49:13 -0500

    p

--- src/main.ts
+++ src/main.ts
@@ -3,8 +3,12 @@ import { AppModule } from './api/app/module/app.module'
 import { ValidationPipe } from '@nestjs/common'
 
 async function main() {
-  const app = await NestFactory.create(AppModule)
-  app.enableCors()
+  const app = await NestFactory.create(AppModule, {
+    cors: {
+      origin: '*',
+      methods: ['GET', 'PUT', 'POST', 'DELETE']
+    }
+  })
   app.useGlobalPipes(new ValidationPipe())
   await app.listen(3000)
 }
